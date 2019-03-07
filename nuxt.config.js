6e725543e0cc0648dae9f172c9c9889ba125ce24
David Freund <dev@davidfreund.de>
2019-03-07 11:45:26 +0100

    # Installed font-awesome (pro version, free version should work as well)

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-
+	  	 { src: '~/plugins/fontawesome.js', ssr: false }
 	  ],
 
 	/*
---
+++ plugins/fontawesome.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBus } from '@fortawesome/pro-solid-svg-icons'
+import { faSubway } from '@fortawesome/pro-solid-svg-icons'
+import { faCloudRain } from '@fortawesome/pro-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faBus,faSubway,faCloudRain )
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
